a83699cad644c2afee37248b28c651370c2b7381
luisgaboardi <luis.ggaboardi@gmail.com>
2024-11-20 17:25:04 -0300

    Backend integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    const testIntegration = () => {
+      axios.get(apiUrl).then(response => {
+        console.log(response)
+      })
+    }
+    testIntegration()
+  }, [count])
+  
 
   return (
     <>
