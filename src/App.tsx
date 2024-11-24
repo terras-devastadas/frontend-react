c3e583bd21698a4188ca5c63aeafbc46525abc4d
luisgaboardi <luis.ggaboardi@gmail.com>
2024-11-23 23:39:03 -0300

    Backend integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import "./App.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const apiUrl = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    const testIntegration = () => {
-      axios.get(apiUrl).then(response => {
-        console.log(response)
-      })
-    }
-    testIntegration()
-  }, [count])
-  
-
   return (
     <>
       <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,37 @@
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default HomePage
+const HomePage = () => {
+  const [items, setItems] = useState<any>([]);
+
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    const getItems = () => {
+      axios.get(`${apiUrl}/items/`).then((response) => {
+        setItems(response.data?.results);
+      });
+    };
+    getItems();
+  }, []);
+
+  return (
+    <div>
+      <h1>HomePage</h1>
+
+      <h2>Item do back</h2>
+
+      {items.map((item: any) => {
+        return (
+          <div key={item.id}>
+            <p className="mb-1">
+              {item.id} - {item.name} - {item.description}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HomePage;
