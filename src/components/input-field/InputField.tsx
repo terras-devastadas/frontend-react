6da3c624529a78c3a2721632201060c678156e25
Hugo Freitas Silva <videogame041@gmail.com>
2024-11-25 23:58:25 -0300

    Creates HTTP requests for registration page

--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, {forwardRef} from "react";
 import styles from './InputField.module.css';
-
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     variant?: string;
@@ -8,13 +7,15 @@ interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
 }
 
-const InputField: React.FC<InputFieldProps> = ({variant, htmlFor, label, ...rest}) => {
-    return(
-        <div className={styles.InputContainer}>
-            <label className={styles.InputLabel} htmlFor={htmlFor}>{label}</label>
-            <input className={`${styles.InputArea} ${variant}`} name={htmlFor} {...rest}/> 
-        </div>
-    );
-}
+const InputField = forwardRef<HTMLInputElement ,InputFieldProps>(
+        ({variant, htmlFor, label, type = "text", ...rest}, ref) => {
+        return(
+            <div className={styles.InputContainer}>
+                <label className={styles.InputLabel} htmlFor={htmlFor}>{label}</label>
+                <input className={`${styles.InputArea} ${variant}`} name={htmlFor} ref={ref} type={type} {...rest}/> 
+            </div>
+        );
+    }
+);
 
 export default InputField
--- src/pages/registration-page/RegistrationPage.tsx
+++ src/pages/registration-page/RegistrationPage.tsx
@@ -1,18 +1,36 @@
-//import {useState} from 'react';
+import {useRef} from 'react';
 import InputField from "../../components/input-field/InputField";
 import "./StyleRegistrationPage.css";
 
 
 const RegistrationPage = () => {
+    const apiURL = import.meta.env.VITE_API_URL;
+
+    const inputName = useRef<HTMLInputElement>(null);
+    const inputLastName = useRef<HTMLInputElement>(null);
+    const inputNickName = useRef<HTMLInputElement>(null);
+    const inputEmail = useRef<HTMLInputElement>(null);
+    const inputPassword = useRef<HTMLInputElement>(null);
+
+    async function registerUsers(){
+        await apiURL.post('/usuarios', {
+            name: inputName.current?.value,
+            lastName: inputLastName.current?.value,
+            nickName: inputNickName.current?.value,
+            email: inputEmail.current?.value,
+            password: inputPassword.current?.value
+        })
+    }
+
     return(
         <div className="registration-form">
             <h2 className="registration-title">CADASTRO</h2>
-            <InputField htmlFor="name" label="Nome:"></InputField>
-            <InputField htmlFor="lastName" label="Sobrenome:"></InputField>
-            <InputField htmlFor="nickname" label="Apelido:"></InputField>
-            <InputField type="email" htmlFor="email" label="E-mail institucional:" ></InputField>
-            <InputField type="password" htmlFor="password" label="Senha:" ></InputField>
-            <button type="submit" className="registration-button">Criar Conta</button>
+            <InputField htmlFor="name" label="Nome:" ref={inputName} required></InputField>
+            <InputField htmlFor="lastName" label="Sobrenome:" ref={inputLastName}></InputField>
+            <InputField htmlFor="nickname" label="Apelido:" ref={inputNickName}></InputField>
+            <InputField type="email" htmlFor="email" label="E-mail institucional:" ref={inputEmail}></InputField>
+            <InputField type="password" htmlFor="password" label="Senha:" ref={inputPassword}></InputField>
+            <button type="submit" className="registration-button" onClick={registerUsers}>Criar Conta</button>
         </div>
     )
 }
