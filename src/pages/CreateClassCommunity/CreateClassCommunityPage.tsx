8e2bd8b0e08810e6ed80b35435f9d2e54a0b0e31
Hugo Freitas Silva <videogame041@gmail.com>
2024-12-08 23:58:46 -0300

    update styles

--- src/pages/CreateClassCommunity/CreateClassCommunityPage.tsx
+++ src/pages/CreateClassCommunity/CreateClassCommunityPage.tsx
@@ -6,24 +6,26 @@ const CreateClassCommunityPage = () => {
 
     return(
         <>
-            <div className={styles.Container}>
+            <div className={styles.container}>
                 <h2 className={styles.title}>Criação de Comunidade</h2>
-                <div className={styles.leftSide}>
-                    <InputField />
-                    <TextField />
-                </div>
+                <div className={styles.containerContent}>
+                    <div className={styles.leftSide}>
+                        <InputField label='Nome da comunidade:'/>
+                        <TextField label='Descrição:'/>
+                    </div>
 
-                <div className={styles.rightSide}>
-                    <div className={styles.visibilityContainer}>
-                        <h3 className={styles.visibilityTitle}>Visibilidade:</h3>
-                        <button className={styles.visibilityOption}>Professores</button>
-                        <button className={styles.visibilityOption}>Todos</button>
-                    </div>
-                    <div className={styles.bannerContainer}>
-                        <label htmlFor="fileInput" className={styles.bannerLabel}>Banner:</label>
-                        <input type="file" id="fileInput" name="Banner" accept="image/*" className={styles.bannerInput}/>
-                    </div>
-                    <button type='submit' className={styles.submitButton}>Criar Comunidade</button>
+                    <div className={styles.rightSide}>
+                        <div className={styles.visibilityContainer}>
+                            <h3 className={styles.visibilityTitle}>Visibilidade:</h3>
+                            <button className={styles.visibilityOption}>Professores</button>
+                            <button className={styles.visibilityOption}>Todos</button>
+                        </div>
+                        <div className={styles.bannerContainer}>
+                            <label htmlFor="fileInput" className={styles.bannerLabel}>Personalizar:</label>
+                            <input type="file" id="fileInput" name="Banner" accept="image/*" className={styles.bannerInput}/>
+                        </div>
+                        <button type='submit' className={styles.submitButton}>Criar Comunidade</button>
+                    </div> 
                 </div>
             </div>
         </>
