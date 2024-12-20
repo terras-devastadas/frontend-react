86e872cf520c62b4727aa03e2b891646a263d5b1
luisgaboardi <luis.ggaboardi@gmail.com>
2024-12-20 20:40:19 -0300

    Integrate view profile page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App.tsx";
 import HomePage from "./pages/Home/HomePage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import LoginPage from "./pages/Login/LoginPage.tsx";
 import RegistrationPage from "./pages/Registration/RegistrationPage.tsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import ExibirPerfil from "./pages/ExibirPerfil/ExibirPerfil.tsx";
+import ProfilePage from "./pages/Profile/ProfilePage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -30,8 +30,8 @@ const router = createBrowserRouter([
         element: <RegistrationPage />,
       },
       {
-        path: "exibir-perfil",
-        element: <ExibirPerfil />,
+        path: "perfil",
+        element: <ProfilePage />,
       },
     ],
   },
