17651afb8c2cb14348536c6417075ad4bf08ca89
Hugo Freitas Silva <videogame041@gmail.com>
2025-01-26 23:12:25 -0300

    feat: creates main structure of HomePage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,17 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "../../services/api";
+import styles from "./HomePage.module.css";
+
 
 const HomePage = () => {
+  const[posts, setPosts] = useState<any>()
+
   useEffect(() => {
-    const testIntegration = () => {
-      api.get("posts/").then((response) => {
-        console.log(response);
-      });
-    };
-    testIntegration();
+    async function getPosts() {
+      try{
+        const response = await api.get("/posts/")
+        // console.log(response.data)
+        setPosts(response.data)
+
+      } catch (error) {
+        console.error("Erro ao buscar posts:", error);
+      }
+    }
+    getPosts();
   }, []);
 
-  return <div>HomePage</div>;
+  // O usuário precisa estar logado para acessar essa página
+  // Verificar se o token está presente no localStorage
+  if(!posts) {
+    return <div>Carregando...</div>
+  }
+
+  return (
+    <>
+      {posts && posts.map((post: any) => (
+      <div className={styles.post} key={post.id}>
+        <h2 className={styles.title}>{post.title}</h2>
+        <p className={styles.content}>{post.content}</p>
+        {post.image? 
+          <div>
+            <img src={post.image} alt="Imagem do post" className={styles.image}/> 
+          </div> 
+        : null}
+      </div>
+      ))}
+    </>
+  );
 };
 
 export default HomePage;
