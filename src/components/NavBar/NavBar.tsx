84bf795e5d32c4a9170def912762dde446cc4d7e
thetrulyvoid <arthurmss2006@gmail.com>
2025-01-28 08:55:05 -0300

    Search Bar (Design 100% + Função 100% + Falta a página de resultados)

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,21 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+// src/components/NavBar.js
+import { Navbar, Container, Nav, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import styles from "./NavBar.module.css";
 import lasagneLogo from "../../assets/lasagneLogo.png";
 
 const NavBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  // Função para lidar com a pesquisa
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Redireciona para a página de busca passando o termo na URL
+    navigate(`/search?query=${searchTerm}`);
+  };
+
   return (
     <Navbar expand="sm" className={styles.navbar}>
       <Container>
@@ -13,7 +26,7 @@ const NavBar = () => {
             width="30"
             height="30"
             className="d-inline-block align-top"
-          />{" "}
+          />
           <span>LaSagne</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -44,6 +57,18 @@ const NavBar = () => {
               Criar post
             </Nav.Link>
           </Nav>
+
+          {/* Barra de Pesquisa */}
+          <Form className="d-flex" onSubmit={handleSearch}>
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className={styles["search-bar"]}
+              aria-label="Search"
+            />
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
