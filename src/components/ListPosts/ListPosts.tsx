3ca47593dfcecbb0332c21cf23a51503016f3c26
Hugo Freitas Silva <videogame041@gmail.com>
2025-02-05 22:50:00 -0300

    fix: title and user photo

--- src/components/ListPosts/ListPosts.tsx
+++ src/components/ListPosts/ListPosts.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import api from "../../services/api";
 import styles from "./ListPosts.module.css";
 import React from "react";
+import Avatar from "../../assets/AvatarPlaceholder.png";
 // import { Link } from "react-router-dom";
 
 interface ListPostsProps {
@@ -34,7 +35,22 @@ const ListPosts: React.FC<ListPostsProps> = ({endpoint}) => {
     <>
       {posts && posts.map((post: any) => (
       <div className={styles.post} key={post.id}>
-        <h2 className={styles.title}>{post.title}</h2>
+        <div className={styles.infoUser}>
+          {post.user.photo_profile? 
+          <div className={styles.userPhotoContainer}>
+            <img src={post.user.photo_profile} alt="Foto de perfil do autor" title={`@${post.user.username}`} className={styles.userPhoto}/> 
+          </div> 
+          : 
+          <div className={styles.userPhotoContainer}>
+            <img src={Avatar} alt="Foto de perfil do autor" title={`@${post.user.username}`} className={styles.userPhoto}/> 
+          </div> 
+          }
+
+          <div className={styles.containerTitle}><h2 className={styles.title}>{post.title}</h2></div>
+        </div>
+
+
+        {/* Adicionar em qual comunidade o post foi publicado */}
         <p className={styles.content}>{post.content}</p>
         {post.image? 
           <div>
